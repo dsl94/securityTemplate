1b2e62dd240c0e1539752cc7b58710c6d52a010c
Nemanja Petrovic <nemanjabosnjane@gmail.com>
2017-09-30 18:01:44 +0200

    Added cors filter

---
+++ src/main/java/com/homeservices/homeservices/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.homeservices.homeservices.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by nemanja on 9/30/2017.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
