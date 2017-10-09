8dd13d0fe6932c08dee7236a2f1c84d3af81eef2
Nemanja Petrovic <nemanjap@niri.rs>
2017-10-09 14:22:52 +0200

    Cors filter

--- src/main/java/com/homeservices/homeservices/security/WebSecurityConfig.java
+++ src/main/java/com/homeservices/homeservices/security/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
         // will fail with 403 Invalid CORS request
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
